ca3b615c982bd7985b3d1a1c14d74fdc2969e5b7
Michael Critelli <mcritelli25@aol.com>
2015-07-28 23:40:18 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pod::Application.config.secret_key_base = '99ceef6014c63baa6f35f120bb178d33453363843909e94439082da5cba45b30ae44b9e61e2b613d23bae1b0fd7c9d70c8293c586d5e31787ebac29061dd3387'
