3d90e2d8e302b151a17eb74cece66fdf63b74f74
Michael Critelli <mcritelli25@aol.com>
2015-07-29 13:49:57 +0000

    Linked Submit Button To Main Page

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,9 +1,21 @@
 class InputsController < ApplicationController
-	def index
-	 @inputs = Input.all
-	end
+  def index
+   @inputs = Input.all
+  end
 
-	def new
-	 @input = Input.new
-	end
+  def new
+   @input = Input.new
+  end
+
+  def create
+   Input.create(input_params)
+   redirect_to root_path
+  end
+
+  private
+
+  def input_params
+   params.require(:input).permit(:circuit, :start_run, :end_run, :estimated_trench, :estimated_slack, :completion_date, :cable_types)
+  end
+
 end
